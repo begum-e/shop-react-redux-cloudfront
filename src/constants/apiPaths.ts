e3aa24e854e1c22fd49460919987f51c658f5fc2
Begum <110823067+begum-e@users.noreply.github.com>
2022-11-06 18:30:03 +0300

    Cart Api Path is updated with new dockerized app from elasticbeanstalk api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://t90mu4k2ze.execute-api.eu-west-1.amazonaws.com/dev/api",
+  cart:"http://begum-e-cart-api-develop.eba-yspbmped.eu-west-1.elasticbeanstalk.com"
+ // cart: "https://t90mu4k2ze.execute-api.eu-west-1.amazonaws.com/dev/api",
 };
 
 export default API_PATHS;
