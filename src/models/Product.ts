ff8a367ca54032f1390095c81e43f7bf4f81f453
Begum <10823067+begum-e@users.noreply.github.com>
2022-11-14 00:22:57 +0300

    bff service api, products image updated

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,6 +5,9 @@ export const ProductSchema = Yup.object({
   title: Yup.string().required().default(""),
   description: Yup.string().default(""),
   price: Yup.number().positive().required().defined().default(0),
+  count: Yup.number().default(0),
+  imageUrl: Yup.string(),
+
 });
 
 export const AvailableProductSchema = ProductSchema.shape({
